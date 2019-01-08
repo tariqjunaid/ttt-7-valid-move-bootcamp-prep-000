e6f8e4d1c754d42c0c87bf011e1320f4943c1bb0
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-01-08 23:34:08 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,4 +7,17 @@ def valid_move?(board, index)
     end
   end
   
-  def on_board()
+  def on_board(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+  
+  if (position_taken?(board, index)) == false && (on_board(num)) == true
+    return true
+  else
+    return false
+  end
+end
