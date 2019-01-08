413f7b1cce41afe86f67c7e9e5c7a212c5cddbcc
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-01-08 23:20:30 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,15 +1,9 @@
 def valid_move?(board, index)
-  if position_taken? == false
-    return true
-  else
-    return false
-  end
-end
-
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    return false
-  else
-    return true
+  def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      return false
+    else
+      return true
+    end
   end
 end
